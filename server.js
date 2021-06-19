affbd97be7b415b3bd766bd07894bbbe8c9dc814
Cat <morancathy@gmail.com>
2021-06-19 10:31:35 -0500

    created template and root route

---
+++ server.js
@@ -0,0 +1,60 @@
+//load express
+const express = require('express');
+//create express app
+const app = express();
+const PORT = 3000;
+
+// Database setup
+
+
+//congiure the app (app.set)
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+//mount middleware (app.use)
+app.use((req, res, next) =>{
+  console.log('HEEEEYYYYY I am middleware, running before all routes');
+  next()
+});
+
+app.use(express.urlencoded({extended: true}));  //use for body parser?
+
+/*****************
+Mount INDUCES Routes
+******************/
+/*
+Index
+*/
+//define a "root" route directly on app. This is callback function AKA route handler
+app.get('/', (req, res) => {
+  res.send("This is veggie app")
+})
+/*
+New         //Just renders the page. SHows the form to input data
+*/
+
+/*
+Delete
+*/
+/*
+Update
+*/
+/*
+Create      //Recieves the info and pushes the info. Action that actually posts the data to server
+*/
+
+/*
+Edit
+*/
+/*
+Show
+*/
+
+
+
+
+
+//tell app to listen on port 3000 for HTTP requests from clients
+app.listen(PORT, () => {
+  console.log(`Listening in on port: ${PORT}`);
+})
